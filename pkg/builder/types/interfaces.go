eec3b144a85d40b170dc55354684ddb92429e914
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2021-10-11 17:13:02 +0200

    Fix BuilderInterface

--- pkg/builder/types/interfaces.go
+++ pkg/builder/types/interfaces.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2019 The Kubernetes Authors All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,13 +41,14 @@ type BuilderInterface interface {
 	WithGenerateStoresFunc(f BuildStoresFunc)
 	DefaultGenerateStoresFunc() BuildStoresFunc
 	Build() []metricsstore.MetricsWriter
+	BuildStores() [][]cache.Store
 }
 
-// BuildStoresFunc function signature that is used to return a list of metricsstore.MetricsStore
+// BuildStoresFunc function signature that is used to return a list of cache.Store
 type BuildStoresFunc func(metricFamilies []generator.FamilyGenerator,
 	expectedType interface{},
 	listWatchFunc func(kubeClient clientset.Interface, ns string) cache.ListerWatcher,
-) []*metricsstore.MetricsStore
+) []cache.Store
 
 // AllowDenyLister interface for AllowDeny lister that can allow or exclude metrics by there names
 type AllowDenyLister interface {
